fa450ce886bdcdb2abc07934642bc715f9eb20eb
Simion Okola <okola.simion168@gmail.com>
2024-02-02 13:57:07 +0300

    remove double migration

--- db/migrate/20240124204823_add_columns_to_completed_trucks.rb
+++ db/migrate/20240124204823_add_columns_to_completed_trucks.rb
@@ -2,6 +2,6 @@ class AddColumnsToCompletedTrucks < ActiveRecord::Migration[6.1]
   def change
     add_column :completed_trucks, :spare, :string
     add_column :completed_trucks, :status, :string
-    add_column :completed_trucks, :completed_at, :datetime
+    # add_column :completed_trucks, :completed_at, :datetime
   end
 end
