aa01fb8c460332af872a7ccdba1300bce2b10c65
Simion Okola <okola.simion168@gmail.com>
2024-02-14 22:04:51 +0300

    delays

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -7,8 +7,8 @@ class TrucksController < ApplicationController
 
 
   def index
-    qualified_trucks = Truck.all.select { |truck| truck_qualifies_for_allowance?(truck) }
-    render json: qualified_trucks
+    trucks = Truck.all
+    render json: trucks
   end
 
   def show
@@ -19,49 +19,8 @@ class TrucksController < ApplicationController
         render json: { error: "Not authorized" }, status: :unauthorized
     end
 end
-
-def calculate_allowance_details(truck)
-  days_since_departure = (Date.today - truck.departureDate).to_i
-  allowance_qualifying_days = truck.allowanceQualifyingDays
-
-  if days_since_departure >= allowance_qualifying_days
-    days_to_be_paid = days_since_departure - allowance_qualifying_days + 1
-    unpaid_days = [0, days_to_be_paid - truck.paidDelayDays].max
-    paid_days = [0, [truck.paidDelayDays, days_to_be_paid].min].max
-    excess_days_paid = [0, truck.paidDelayDays - days_to_be_paid].max
-
-    {
-      truckNumber: truck.truckNumber,
-      driverName: truck.driverName,
-      daysSinceDeparture: days_since_departure,
-      daysToBePaid: days_to_be_paid,
-      paidDelayDays: paid_days,
-      unpaidAllowanceDays: unpaid_days,
-      excessDaysPaid: excess_days_paid
-    }
-  else
-    {
-      message: "Driver does not qualify for allowance yet."
-    }
-  end
-end
-  
-  def delay_entry
-    if @truck.update(truck_params)
-      render json: calculate_allowance_details(@truck)
-    else
-      render json: @truck.errors, status: :unprocessable_entity
-    end
-  end
-  
   private
 
-  def truck_qualifies_for_allowance?(truck)
-    days_since_departure = (Date.today - truck.departureDate).to_i
-    allowance_qualifying_days = truck.allowanceQualifyingDays
-    days_since_departure >= allowance_qualifying_days
-  end
-  
   def truck_params
     params.require(:truck).permit(:paidDelayDays, :truckNumber, :driverName, :departureDate, :allowanceQualifyingDays, :paidDelayDays,)
   end
