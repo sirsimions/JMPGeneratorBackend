231848bccf247bbfe8d952811a0587352e699f0c
Simion Okola <okola.simion168@gmail.com>
2024-02-15 23:24:01 +0300

    delay form

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -19,10 +19,27 @@ class TrucksController < ApplicationController
         render json: { error: "Not authorized" }, status: :unauthorized
     end
 end
+
+def delay_entry
+  truck = Truck.find_by(truckNumber: params[:id])
+  if truck
+    if truck.update(truckpay_params)
+      render json: truck, status: :ok
+    else
+      render json: { error: "Failed to update truck's delay days" }, status: :unprocessable_entity
+    end
+  else
+    render json: { error: "Truck not found" }, status: :not_found
+  end
+end
   private
 
   def truck_params
     params.require(:truck).permit(:paidDelayDays, :truckNumber, :driverName, :departureDate, :allowanceQualifyingDays, :paidDelayDays,)
+  end
+
+  def truckpay_params
+    params.require(:truck).permit(:paidDelayDays)
   end
   end
   
